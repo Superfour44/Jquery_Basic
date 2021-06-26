20168c88d82bac4f33f15c536fc7b0ef08c1b643
Sutthatip Kanchanapradit <sutthatip4@gmail.com>
2021-06-27 00:29:22 +0700

    learn basic jquery
    effect event

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    //---------------------------------Jquery Effect--------------------------------
+
+    $('#show').click(function(){
+        $('p').toggle(2000);//hide and show
+    });
+
+    // $('#show').click(function(){
+    //     $('p').show(2000); //2000 millisecond
+    // });
+
+    // $('#hide').click(function(){
+    //     $('p').hide(2000);
+    // });
+
+    //---------------------------------Jquery Effect--------------------------------
+
+
+    // $('p').on({
+    //     mouseenter: function(){
+    //         $(this).css('background-color',"lightgray");
+    //     },
+    //     mouseleave: function(){
+    //         $(this).css('background-color',"lightblue")
+    //     },
+    //     click:function(){
+    //         $(this).css('background-color',"yellow")
+    //     }
+    // });
+
+    // jquery event click
+    // $('#btn').click(function(){ 
+    //     $("p").hide();
+    // });
+
+    // jquery event double click
+    // $('#btn').dblclick(function(){ 
+    //     $("p").hide();
+    // });
+
+    // jquery event mouse enter --> mouse move in object is enter 
+    // $('#btn').mouseenter(function(){ 
+    //     $("p").hide();
+    // });
+
+    // jquery event mouse leave --> mouse move on object is enter 
+    // $('#btn').mouseleave(function(){ 
+    //     $("p").hide();
+    // });
+    //---------------------------------Jquery Event--------------------------------
+
+
+});
